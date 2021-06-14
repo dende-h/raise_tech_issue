303f7651127126c955420f19dcf29693891df3bb
dende-h <miyuki_dende@yahoo.co.jp>
2021-06-14 15:55:41 +0900

    出力の際の文章を追加 課題3のコードを書き換え

--- RaiseTechIsseu/src/issue/tech/raise/Hello_world.java
+++ RaiseTechIsseu/src/issue/tech/raise/Hello_world.java
@@ -1,11 +1,12 @@
 package issue.tech.raise;
 
 
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 		public class Hello_world {
 
@@ -14,47 +15,47 @@ import java.util.List;
 				
 				//課題1コード
 				String greeting = "HelloWorld";
-				System.out.println(greeting);
+					System.out.println("課題1の出力");
+					System.out.println("「世界よこんにちは」を英語で" + "“" + greeting + "”");
+					System.out.println("******************************");//見やすいように
 				//課題2コード
 				int s = 0;
 				for(int i = 1; i < 11 ; i++) {
 					 s += i;
 				}
-				System.out.println(s);
+					System.out.println("課題2の出力");
+					System.out.println("1から10までの総和は" + s + "です");
+					System.out.println("******************************");
+				//課題３コード	
+					System.out.println("課題3の出力");
+				LocalDateTime local = LocalDateTime.now();
+				DateTimeFormatter fomatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日(E)",Locale.JAPANESE);
+				local = local.plusYears(3);
+			    String format = local.format(fomatter);
+			    	System.out.println("3年後の今日は" + format + "です");
+			    	System.out.println("******************************");
 			
-			//課題３コード	
-			LocalDateTime local = LocalDateTime.now();
-				System.out.println(local.plusYears(3));
-			DayOfWeek dayWeek = local.getDayOfWeek();
-				System.out.println(dayWeek);
-			
-			//課題４コード
-			Judgment.doIf(1);
-			Judgment.doIf(0);
-			
-			//課題５のコード
-			List<String> list = new ArrayList<String> (Arrays.asList("T-REX","Spino saurus","Triceratops")) ;
-			
-				for(int i = 0; i < list.size(); i++) {
-					System.out.println(list.get(i));
-				}
-
-			
+			    //課題４コード
+			    	System.out.println("課題4の出力");
+			    Judgment.doIf(1);
+			    Judgment.doIf(0);
+					System.out.println("******************************");
+				//課題５のコード
+				List<String> list = new ArrayList<String> (Arrays.asList("T-REX","Spino saurus","Triceratops")) ;
+					System.out.println("課題5の出力");
+					for(int i = 0; i < list.size(); i++) {
+						System.out.println(list.get(i));
+					}
 			}
-
 		}
-		 class Judgment{	
+		class Judgment{	
 			public static void doIf(int i) {
 				if(i == 0) {
-					System.out.println("偽");
+					System.out.println("引数が0の場合は偽");
 				}else if(i == 1) {
-					System.out.println("真");
+					System.out.println("引数が1の場合は真");
 				}
 			}
-
-		
-		
-			
-	}
+		}
 
 
